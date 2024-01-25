e2c48fdb6c34dcaf8e014b8a7dc35b7bfaa5c99c
AuenKr <auenkumar64@gmail.com>
2024-01-25 14:07:40 +0530

    change search bar

--- src/components/SearchField.jsx
+++ src/components/SearchField.jsx
@@ -11,10 +11,10 @@ export default function SearchField({ setItem }) {
         setItem(input);
     }
     return (
-        <div>
+        <div className='sticky top-0 z-10'>
             <form id='searchBar' name='searchBar' className='flex justify-center'>
                 <input value={input} onChange={updateInput} className='border-2 border-black rounded-lg m-1 pl-2 hover:bg-slate-100' />
-                <button onClick={searchItem} className='flex items-center border-2 border-gray-700 rounded-lg m-1 p-1 hover:bg-slate-300 '>
+                <button onClick={searchItem} className='flex items-center border-2 border-gray-700 rounded-lg m-1 p-1 bg-slate-100 hover:bg-slate-300 '>
                     Search {search}
                 </button>
             </form>
